5f92395445d2124c2e04ac85e1c056aea3f684db
eddiemuhoro <eddiemuhoro@gmail.com>
2025-04-12 21:19:55 +0300

    update .gitignore to exclude environment and cache files; refactor database connection URL to use environment variable

--- app/database.py
+++ app/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
-DIRECT_URL="postgresql+asyncpg://postgres.mmiyafqvgmknxbqeepha:Edwin.4967%@aws-0-eu-central-1.pooler.supabase.com:5432/postgres"
+DIRECT_URL=os.getenv("DIRECT_URL")
 
 engine = create_async_engine(DIRECT_URL, echo=True, future=True)
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
