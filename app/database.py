76ff2f652d2354bc78fcc4a801993ff54f27efa6
eddiemuhoro <eddiemuhoro@gmail.com>
2025-04-13 16:23:51 +0300

    load environment variables from specific path and log database connection URL

--- app/database.py
+++ app/database.py
@@ -3,8 +3,9 @@ from dotenv import load_dotenv
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-load_dotenv()
+load_dotenv(dotenv_path="app/.env")
 DIRECT_URL=os.getenv("DIRECT_URL")
+print(f"Connecting to database at {DIRECT_URL}")
 
 engine = create_async_engine(DIRECT_URL, echo=True, future=True)
 SessionLocal = sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
